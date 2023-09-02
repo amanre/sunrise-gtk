9f96307310f8b938aa5b37ec301f8a041afc548a
amanre <amanre556@gmail.com>
2023-09-02 03:32:52 +0200

    update

--- 1-build.sh
+++ 1-build.sh
@@ -5,7 +5,7 @@ destination="/home/amanre/custom_repo/x86_64/"
 
 			makepkg -f
 
-			mv sunrise-gtkl-git*pkg.tar.zst $destination
+			mv sunrise-gtk-git*pkg.tar.zst $destination
 
 			rm -r pkg
 			rm -r src
